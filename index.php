5bec1f74f028b331563848d0d50d2d86e73e69f6
alejandrocifuent <luisparra5380@gmail.com>
2024-09-04 22:58:47 -0500

    refactor(index.php): definitive change of the design

--- index.php
+++ index.php
@@ -28,7 +28,6 @@
       left: 0;
       right: 0;
       z-index: 50; /* Asegura que el navbar esté por encima de otros elementos */
-      background: rgba(0, 0, 0, 0.5); /* Transparente con fondo negro */
     }
   </style>
 </head>
@@ -36,77 +35,106 @@
 <body class="bg-black min-h-screen relative overflow-hidden">
   <!-- Navbar -->
   <div class="navbar bg-transparent py-4"> <!-- Navbar transparente -->
-    <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle text-xl" data-aos="zoom-in">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
-          </svg>
-        </div>
-        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          data-aos="zoom-in">
-          <li><a class="text-lg">Inicio</a></li>
-          <li><a class="text-lg">Perfil</a></li>
-          <li><a class="text-lg">Contacto</a></li>
-        </ul>
+  <div class="navbar-start flex justify-center">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
       </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Inicio</a></li>
+        <li>
+          <a>Servicios</a>
+          <ul class="p-2">
+            <li><a>Escuelas</a></li>
+            <li><a>Cursos</a></li>
+          </ul>
+        </li>
+        <li><a>Nosotros</a></li>
+      </ul>
     </div>
-    <div class="navbar-center" data-aos="zoom-in">
-      <a class="btn btn-ghost text-3xl font-bold">CLASSMENT ACADEMY</a>
-    </div>
-    <div class="navbar-end">
-      <div>
-        <?php if(isset($_SESSION['usuario_documento'])): ?>
-        <a href="perfil_usuario.php" class="flex items-center space-x-2 text-xl" data-aos="zoom-in">
+    <a href="index.php" 
+    class="btn btn-ghost ">
+      <img src="../IMG/logo.png" alt="logo" 
+      class="w-f h-full" data-aos="zoom-in">
+      <h1 class="text-2xl font-bold" data-aos="zoom-in">Classment Academy</h1>
+    </a>
+  </div>
+  <div class="navbar-center hidden lg:flex" data-aos="zoom-in">
+    <ul class="menu menu-horizontal px-1" data-aos="zoom-in">
+      <li><a>Inicio</a></li>
+      <li>
+        <details>
+          <summary>Servicios</summary>
+          <ul class="p-2">
+            <li><a>Escuelas</a></li>
+            <li><a>Cursos</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Nosotros</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <a class="btn-ghost ">
+    <?php if(isset($_SESSION['usuario_documento'])): ?>
+        <a href="perfil_usuario.php" class="flex items-center space-x-2 text-xl pr-32" data-aos="zoom-in">
           <span>Mi Perfil</span>
           <i class="fas fa-user-circle text-3xl"></i>
         </a>
         <?php else: ?>
-        <a href="login.php" class="flex items-center space-x-2 text-xl" data-aos="zoom-in">
+        <a href="login.php" class="flex items-center space-x-2 text-xl pr-32" data-aos="zoom-in">
           <span>Ingresa</span>
           <i class="fas fa-sign-in-alt text-3xl"></i>
         </a>
         <?php endif; ?>
+    </a>
+    
+
+  </div>
+</div>
+      
       </div>
     </div>
   </div>
 
   <!-- Sección Hero -->
-  <section class="hero-section flex items-center justify-center relative">
+  <section class="hero-section flex items-center justify-center relative pl-24" data-aos="fade-up">
     <!-- Círculos de gradiente -->
-    <div class="gradient-circle w-32 h-32 bg-green-400 top-10 left-10 opacity-50"></div>
-    <div class="gradient-circle w-64 h-64 bg-purple-400 top-10 right-10 opacity-30"></div>
-    <div class="gradient-circle w-64 h-64 bg-pink-400 bottom-10 left-10 opacity-30"></div>
-    <div class="gradient-circle w-64 h-64 bg-orange-400 bottom-10 right-10 opacity-30"></div>
-    <div class="gradient-circle w-64 h-64 bg-blue-400 bottom-10 right-1/4 opacity-30"></div>
+    <div class="gradient-circle w-32 h-32 bg-orange-300 top-10 left-10 opacity-50"></div>
+    <div class="gradient-circle w-64 h-64 bg-orange-400 top-10 right-10 opacity-30"></div>
+    <div class="gradient-circle w-64 h-64 bg-orange-300 bottom-10 left-10 opacity-30"></div>
+    <div class="gradient-circle w-64 h-64 bg-orange-300 bottom-10 right-1/4 opacity-30"></div>
 
-    <div class="container mx-auto px-4 z-10">
-      <div class="text-center mb-12">
-        <h1 class="text-4xl md:text-6xl font-bold text-white mb-4 "data-aos="zoom-in-down">Despierta el talento<br>que llevas dentro</h1>
-        <p class="text-xl text-gray-300 mb-8 "data-aos="zoom-in-down">Sumérgete en esta gran aventura y sácale el mayor provecho a tu instinto
-          deportivo.</p>
-        <button class="btn bg-gradient-to-br from-purple-700 to-blue-700 btn-lg "data-aos="zoom-in-down">ÚNETE</button>
-      </div>
+    <div class="hero-content flex-col lg:flex-row-reverse pr-48">
+    <img
+      src="../IMG/design/fisoculturista.png"
+      class="max-w-sm pr-10 pl-24" data-aos="zoom-in" />
+      <div>
+      <h1 class="text-1xl font-bold tracking-wide leading-normal text-orange-300" data-aos="zoom-in">Tu mejor aliado para aprender</h1>
+      <h1 class="text-6xl tracking-wide leading-snug font-extrabold" data-aos="zoom-in">NADA ES IMPOSIBLE SI NO LO INTENTAS
+      </h1>
+      <p class="py-6 tracking-wide" data-aos="zoom-in">
+        Sacale el mayor provecho a tu tiempo y tus capacidades fisicas, aprende y mejora
+         tus habilidades blandas, elimina las excusas y las distracciones.
 
-      <div class="flex flex-wrap justify-center gap-8 "data-aos="fade-up">
-        <div class="w-40 h-56 md:w-48 md:h-64 bg-gradient-to-br from-red-500 to-orange-500 rounded-full overflow-hidden">
-          <img src="../IMG/design/corredora.png" alt="Deportista 1" class="w-full h-full object-cover">
-        </div>
-        <div class="w-40 h-56 md:w-48 md:h-64 bg-gradient-to-br from-pink-500 to-purple-500 rounded-full overflow-hidden">
-          <img src="../IMG/design/karateka.png" alt="Deportista 2" class="w-full h-auto object-cover">
-        </div>
-        <div class="w-40 h-56 md:w-48 md:h-64 bg-gradient-to-br from-purple-500 to-blue-500 rounded-full overflow-hidden">
-          <img src="../IMG/design/fisoculturista.png" alt="Deportista 3" class="w-full h-50px object-cover">
-        </div>
-        <div class="w-40 h-56 md:w-48 md:h-64 bg-gradient-to-br from-blue-500 to-cyan-500 rounded-full overflow-hidden">
-          <img src="../IMG/design/yoga.png" alt="Deportista 4" class="w-full h-60px object-cover">
-        </div>
-        <div class="w-40 h-56 md:w-48 md:h-64 bg-gradient-to-br from-cyan-500 to-green-500 rounded-full overflow-hidden">
-          <img src="../IMG/design/futbolista.png" alt="Deportista 5" class="w-full h-full object-cover">
-        </div>
-      </div>
+      </p>
+      <a href="login.php"><button class="btn rounded-lg bg-orange-400"data-aos="zoom-in">Empieza ahora!</button></a>
     </div>
+  </div>
+</div>
   </section>
 
 
