17f6a6736ffaf3722e1fe7f97d964881505db365
Hrts Daniel <penad5742@gmail.com>
2024-09-16 13:28:16 -0500

    perf Terms And  Conditions Modals

--- register.php
+++ register.php
@@ -72,8 +72,10 @@
         <div class="text-center">
           <h2 class="font-bold text-xl text-white">Regístrate</h2>
         </div>
+        <div class="max-h-[70vh] overflow-hidden">
+        <div class="pr-4 -mr-4 max-h-[70vh] overflow-y-auto">
         <form action="PHP/register.php" method="post" class="space-y-4">
-          <div class="flex space-x-2">
+          <div class="pl-2 flex space-x-2">
             <div class="flex flex-col w-1/2">
               <label for="usuario_nombre" class="text-sm font-semibold text-orange-400">Nombres</label>
               <input type="text" id="usuario_nombre" name="usuario_nombre" placeholder="Nombres" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
@@ -83,7 +85,7 @@
               <input type="text" id="usuario_apellido" name="usuario_apellido" placeholder="Apellidos" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
             </div>
           </div>
-          <div class="flex space-x-2">
+          <div class="pl-2 flex space-x-2">
             <div class="flex flex-col w-1/2">
               <label for="usuario_correo" class="text-sm font-semibold text-orange-400">Correo Electrónico</label>
               <input type="email" id="usuario_correo" name="usuario_correo" placeholder="Correo Electrónico" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
@@ -93,7 +95,7 @@
               <input type="password" id="usuario_password" name="usuario_password" placeholder="Clave" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
             </div>
           </div>
-          <div class="flex space-x-2">
+          <div class="pl-2 flex space-x-2">
             <div class="flex flex-col w-1/2">
               <label for="usuario_tipo_documento" class="text-sm font-semibold text-orange-400">Tipo de Documento</label>
               <select id="usuario_tipo_documento" name="usuario_tipo_documento" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
@@ -111,7 +113,7 @@
               <input type="text" id="usuario_documento" name="usuario_documento" placeholder="Documento" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
             </div>
           </div>
-          <div class="flex space-x-2">
+          <div class="pl-2 flex space-x-2">
             <div class="flex flex-col w-1/2">
               <label for="usuario_nacimiento" class="text-sm font-semibold text-orange-400">Fecha de Nacimiento</label>
               <input type="date" id="usuario_nacimiento" name="usuario_nacimiento" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
@@ -121,12 +123,12 @@
               <input type="tel" id="usuario_telefono" name="usuario_telefono" placeholder="Teléfono" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white">
             </div>
           </div>
-          <div class="flex flex-col space-y-4">
+          <div class="pl-2 flex flex-col space-y-4">
             <label for="usuario_direccion" class="text-sm font-semibold text-orange-400">Dirección</label>
             <input type="text" id="usuario_direccion" name="usuario_direccion" placeholder="Dirección" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
           </div>
   
-          <div class="flex space-x-4">
+          <div class="pl-2 flex space-x-4">
             <div class="flex flex-col w-1/2">
               <label for="rol_id" class="text-sm font-semibold text-orange-400">Selecciona un rol</label>
               <select name="rol_id" id="rol_id" class="w-full px-4 py-2 bg-gray-900/70 border border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400 text-white" required>
@@ -147,6 +149,34 @@
             </div>
           </div>
   
+          <!-- Términos y Condiciones-->
+          <div class="py-2 flex items-center">
+            <div class="">
+               <input type="checkbox" id="termsAndConditions" class="" required/>
+            </div>
+            <div class="ml-3 text-base">
+                <label for="termsAndConditions" class="text-base text-slate-600">
+                  He leído y autorizo el uso de mis datos en los
+                </label>
+                <a href="#" id="openTermsModal" class="text-base text-orange-400 hover:text-orange-300 no-underline hover:underline">
+                  Términos y Condiciones
+                </a>
+            </div>
+          </div>
+
+          <!-- Modal Terminos y Condiciones -->
+          <dialog id="my_modal_1" class="modal">
+            <div class="modal_box bg-base-300 border-orange-500 p-4 max-w-2xl mx-auto relative shadow-lg">
+              <h3 class="py-2 px-2 text-lg text-orange-500 font-bold">Términos y Condiciones</h3>
+              <div class="border-t-4 border-orange-500"></div>
+              <p class="py-4 px-2 text-sm text-gray-300">Autorizo ​​de manera previa, expresa e informada, como titular de los datos personales comunicados a [Classment Academy] , para el tratamiento de mis datos personales con el fin de: (i) cumplir y hacer cumplir las obligaciones derivadas de la relación entre la Compañía y el titular de los datos, como la gestión de horarios, inscripciones, registro de operaciones, servicio de atención, entre otros; a través de los datos de contacto proporcionados, que incluyen, pero no se limitan a, correo electrónico, número celular, entre otros; (ii) comunicar información relevante sobre los servicios y mejoras que ofrece la Compañía, utilizando medios físicos, digitales y tecnologías de la información como correos electrónicos, redes sociales, mensajes de texto (SMS y/o MMS) y aplicaciones móviles; (iii) evaluar la satisfacción de los usuarios y analizar hábitos de uso para mejorar la gestión del sistema de información; (iv) compartir la información necesaria con terceros encargados de brindar soporte o mejoras en el sistema, bajo los principios de confidencialidad y seguridad de la información.</p>
+              <p class="py-4 px-2 text-sm text-gray-300">El titular de los datos tiene derecho a: (i) conocer, actualizar y rectificar sus datos personales en caso de que sean inexactos, incompletos o estén desactualizados; (ii) solicitar prueba de esta autorización; (iii) ser informado sobre el tratamiento que se le ha dado a sus datos; (iv) presentar quejas ante la Superintendencia de Industria y Comercio en caso de incumplimiento; (v) revocar esta autorización y solicitar la eliminación de sus datos de acuerdo con lo establecido en la Ley 1581 de 2012; (vi) acceder de manera gratuita a los datos proporcionados. Estos derechos podrán ser ejercidos a través de los siguientes canales: [classmentacademy@gmail.com].</p>
+              <div class="modal-action">
+                   <button class="py-2 px-5 bg-black text-white font-semibold rounded-full shadow-md focus:outline-none btn " id="closeModal">Cerrar</button>
+              </div>
+            </div>
+          </dialog>
+
           <button type="submit" class="w-full bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300">
             Enviar
           </button>
@@ -163,6 +193,21 @@
   <script>
     AOS.init({
       duration: 1000
+    });
+    // Script para abrir el modal al hacer clic en el enlace
+    document.getElementById('openTermsModal').addEventListener('click', function (event) {
+      event.preventDefault(); // Previene que el enlace navegue
+      document.getElementById('my_modal_1').showModal(); // Muestra el modal
+    });
+     // Script para abrir el modal
+     document.getElementById('openTermsModal').addEventListener('click', function (event) {
+      event.preventDefault(); // Previene la navegación
+      document.getElementById('my_modal_1').showModal(); // Muestra el modal
+    });
+
+    // Script para cerrar el modal
+    document.getElementById('closeModal').addEventListener('click', function () {
+      document.getElementById('my_modal_1').close(); // Cierra el modal
     });
   </script>
 </body>
