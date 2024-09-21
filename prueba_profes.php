9abfed7e023c129034964c8c2cde83f90c3a8d85
alejandrocifuent <luisparra5380@gmail.com>
2024-09-20 23:47:34 -0500

    perf(querys)schools-courses

--- prueba_profes.php
+++ prueba_profes.php
@@ -1,6 +1,7 @@
 <?php   
 require_once 'PHP/conexion.php';
 
+// Consulta para obtener los datos de los profesores
 $profesores_query = "SELECT p.*, es.escuela_nombre, c.curso_nombre FROM profesores_cursos p
 INNER JOIN escuelas es ON p.escuela_id = es.escuela_id
 INNER JOIN cursos c ON p.curso_id = c.curso_id";
@@ -19,5 +20,23 @@ foreach ($profesores as $profesor) {
     echo "<pre>";
     print_r($profesores_array);
     echo "</pre>";
+
+     //consulta sql
+     $escuelas_query = "SELECT e.*, c.curso_nombre FROM escuelas e 
+     INNER JOIN cursos c ON c.escuela_id = e.escuela_id
+     WHERE escuela_estado = :estado";
+     $stmt = $conn->prepare($escuelas_query);
+     $stmt->bindValue(":estado", "activo", PDO::PARAM_STR);
+     $stmt->execute();
+     $escuelaswithcursos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach ($escuelaswithcursos as $escuela) {
+    $escuelas_array = array(
+        "escuela_id" => $escuela['escuela_id'],
+        "curso_nombre" => $escuela['curso_nombre']
+    );
+    echo "<pre>";
+    print_r($escuelas_array);
+    echo "</pre>";
+}
 }
 
